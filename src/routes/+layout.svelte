604c2eb776ee84aa72eda25515290b92aaddb06c
Mark Manders <mmjmanders@gmail.com>
2025-09-22 10:40:00 +0200

    Added favicons (#2)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { asset } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" href={asset('/apple-touch-icon.png')} />
+	<link rel="manifest" href={asset('/site.webmanifest')} />
 </svelte:head>
 
 {@render children?.()}
