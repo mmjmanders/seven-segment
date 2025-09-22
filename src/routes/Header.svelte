7a28bca9e91fe32c24f2a602ffdeae4477fa546c
Mark Manders <mmjmanders@gmail.com>
2025-09-22 16:06:57 +0200

    Initial components (#5)
    * Initial components
    
    * Added entire app
    
    * Added build sha to env

---
+++ src/routes/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const { VITE_APP_VERSION, VITE_BUILD_SHA } = import.meta.env;
+</script>
+
+<header class="relative">
+	<h1 class="h1 text-5xl font-medium">Segment</h1>
+	<span class="absolute font-mono text-xs right-0">v{VITE_APP_VERSION} - {VITE_BUILD_SHA}</span>
+</header>
