c4a35757ac8efb6ebd0608f291f305b4d0fe7f74
Mark Manders <mmjmanders@gmail.com>
2025-09-22 16:37:38 +0200

    Version bump (#8)
    * Version bump
    
    * Static favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,14 @@
 	import '@fontsource-variable/roboto';
 	import '@fontsource-variable/roboto-mono';
 	import '../styles.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
 	import { asset } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-	<link rel="apple-touch-icon" href={appleTouchIcon} />
+	<link rel="icon" href={asset('/favicon.svg')} />
+	<link rel="apple-touch-icon" href={asset('/apple-touch-icon.png')} />
 	<link rel="manifest" href={asset('/site.webmanifest')} />
 </svelte:head>
 
