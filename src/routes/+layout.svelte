6bee17ebed73b5fcab3e254fd07b051c52601b4e
Mark Manders <mmjmanders@gmail.com>
2025-09-24 13:57:17 +0200

    Added title (#10)
    * Added title
    
    * Version bump

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,8 @@
 	import '@fontsource-variable/roboto';
 	import '@fontsource-variable/roboto-mono';
 	import '../styles.css';
-	import { asset } from '$app/paths';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={asset('/favicon.svg')} />
-	<link rel="apple-touch-icon" href={asset('/apple-touch-icon.png')} />
-	<link rel="manifest" href={asset('/site.webmanifest')} />
-</svelte:head>
 
 {@render children?.()}
