a661b56895c1b3447b6f03fdf47f0486be4169f2
Mark Manders <mmjmanders@gmail.com>
2025-09-24 16:11:59 +0200

    Switch nr segments (#11)
    * Initial 14 segments added
    
    * Finalized 14 segments

--- src/routes/Segment.svelte
+++ src/routes/Segment.svelte
@@ -2,12 +2,14 @@
 	import Number from './Number.svelte';
 
 	let entered = $state<number | undefined>(undefined);
-	let normalized = $derived.by(() =>
-		entered != undefined ? Math.floor(entered / (entered >= 10 ? 10 : 1)) : undefined
-	);
+	let nrSegments = $state<'7' | '14'>('7');
 </script>
 
 <div class="flex flex-col gap-y-8">
+	<select bind:value={nrSegments} class="p-2 border border-neutral-400 rounded-md">
+		<option value="7">7</option>
+		<option value="14">14</option>
+	</select>
 	<input
 		bind:value={entered}
 		type="number"
@@ -16,5 +18,8 @@
 		max="9"
 		class="p-2 border border-neutral-400 rounded-md outline-none focus:outline-1 focus:outline-sky-400 focus:border-sky-400 focus:outline-double"
 	/>
-	<Number input={normalized} />
+	<Number
+		input={entered != undefined ? Math.floor(entered / (entered >= 10 ? 10 : 1)) : undefined}
+		nrSegments={parseInt(nrSegments, 10)}
+	/>
 </div>
